b7b5d1b5c09d245034f2a2c2439aee60c495b4a0
STEPANJOBARSIKOV <stepanji90@rambler.ru>
2024-06-29 15:31:37 +0300

    кастомизированная длина пароля

--- password/new_password.py
+++ password/new_password.py
@@ -2,13 +2,14 @@ import random
 import string
 
 def generate_password(length=12):
-    """Генерация случайного пароля заданной длины."""
+    """Генерация случайного пароля заданной длины (не менее 8 символов)."""
+    if length < 8:
+        print("Минимальная длина пароля должна быть 8 символов. Пароль будет установлен длиной 8 символов.")
+        length = 8
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''
-    for i in range(length):
-        password += random.choice(characters)
+    password = ''.join(random.choice(characters) for i in range(length))
     return password
 
 # Пример использования
-password_length = 12  # Вы можете выбрать любую длину пароля
+password_length = int(input("Введите длину пароля (не менее 8): "))
 print("Ваш новый пароль:", generate_password(password_length))
